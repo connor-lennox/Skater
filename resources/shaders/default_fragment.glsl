8f16b3fffca8ae00918fd29fbd9289c9bb8b1f68
Connor Lennox <clennox99@gmail.com>
2026-01-11 18:34:48 -0500

    update default shaders

--- resources/shaders/default_fragment.glsl
+++ resources/shaders/default_fragment.glsl
@@ -1,13 +1,15 @@
 #version 420 core
 
 in vec2 TexCoords;
+in vec4 tintColor;
+
 out vec4 color;
 
 layout(binding = 0) uniform sampler2D image;
 uniform vec4 spriteColor;
 
 void main() {
-    color = texture(image, TexCoords);
+    color = tintColor * texture(image, TexCoords);
     if (color.a < 0.1) {
         discard;
     }
--- resources/shaders/default_vertex.glsl
+++ resources/shaders/default_vertex.glsl
@@ -1,13 +1,16 @@
 #version 420 core
-
 layout(location = 0) in vec2 a_Position;
 layout(location = 1) in vec4 a_Color;
 layout(location = 2) in vec2 a_Uv;
 
+uniform mat4 uProjection;
+
+out vec4 tintColor;
 out vec2 TexCoords;
 
 void main()
 {
-    gl_Position = vec4(a_Position, 1.0, 1.0);
+    gl_Position = uProjection * vec4(a_Position, 0.0, 1.0);
+    tintColor = a_Color;
     TexCoords = a_Uv;
 }
